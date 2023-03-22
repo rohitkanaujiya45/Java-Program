097b2ab991262fa4c05a16b4223db647cfb51bf9
Rohit Kanaujiya <127976490+rohitkanaujiya45@users.noreply.github.com>
2023-03-22 15:27:19 +0530

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class angle
+{
+   public static void main(String arg[])
+{
+   int a,b,area;
+   System.out.println("Enter the value length and base");
+   Scanner rec=new Scanner(System.in);
+   l=rec.nextInt();
+   b=rec.nextInt();
+   area=(a*b)/2; 
+   
+   System.out.println(area);
+
+}
+
+
+}
